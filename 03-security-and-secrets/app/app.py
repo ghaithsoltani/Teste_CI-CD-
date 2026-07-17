56dd9b25bf204b5531d4fc9f731f618d367454d5
ghaithsoltani <ghaithsoltani36@gmail.com>
2026-07-17 14:38:44 +0100

    Updata secret API KEY

--- 03-security-and-secrets/app/app.py
+++ 03-security-and-secrets/app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Application is running with Secrets Management"
+    return f"API KEY: {os.getenv('API_KEY')}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
