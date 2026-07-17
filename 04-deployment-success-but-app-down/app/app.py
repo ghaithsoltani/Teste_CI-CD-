f5baf189219fded7a4e8f93d452d1aef082a607f
ghaithsoltani <ghaithsoltani36@gmail.com>
2026-07-17 15:06:06 +0100

    Add a new pipeline

--- 04-deployment-success-but-app-down/app/app.py
+++ 04-deployment-success-but-app-down/app/app.py
@@ -7,4 +7,4 @@ def home():
     return "Application is running with Secrets Management"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)aa
+    app.run(host="0.0.0.0", port=5000)
